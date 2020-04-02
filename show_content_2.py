94625830008042bbe31e9ebd480214d6d40e306d
Sofie-Berlin <sofie.s@gmx.de>
2020-04-03 00:13:59 +0200

    betrunken macht es mehr Spaß!

--- show_content_2.py
+++ show_content_2.py
@@ -1,4 +1,7 @@
 import sys
-print(open(sys.argv[1], "r").read())
+try:
+    print(open(sys.argv[1], "r").read())
+except IndexError:
+        print ("Schon wieder das Argument vergessen! Es ist echt immer das Selbe mit dir!")
 
 
